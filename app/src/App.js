45610c6d5d0c434c8dbebfbba955a98707646922
Sadaf <sadaf.ahmadi.1998@gmail.com>
2024-05-23 16:40:43 -0700

    canvas initial creation

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useLayoutEffect, useState} from "react";
+import rough from 'roughjs/bundled/rough.esm'
+
+const generator = rough.generator();
 
 function App() {
+  useLayoutEffect(() => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    // ctx.fillStyle = "green";
+    // ctx.fillRect(10, 10, 150, 100);
+    // ctx.strokeRect(200,200,100,100)
+
+    const roughCanvas = rough.canvas(canvas);
+    const rect = generator.rectangle(10,10,100,100);
+    roughCanvas.draw(rect);
+
+  }
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas id='canvas' width={window.innerWidth} height={window.innerHeight}>
+      canvas
+    </canvas>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>
+    //       Hello world
+    //     </p>
+    //   </header>
+    // </div>
   );
 }
 
