634c4c9f55e3a4b04d040fc7fff9b7f8f87bd529
Sadaf <sadaf.ahmadi.1998@gmail.com>
2024-05-23 20:32:27 -0700

    route installing

--- app/src/index.js
+++ app/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import login from './pages/login';
+import {createBrowserRouter, RouterProvider, Route,} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+  },
+  ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    // <RouterProvider router={router}/>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
