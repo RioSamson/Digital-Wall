46baa1d0cc94bb0183943279035eeec3e0a03a50
Sadaf <sadaf.ahmadi.1998@gmail.com>
2024-07-19 05:21:28 +0200

    updated ai prompt ui

--- app/src/components/PromptModal.jsx
+++ app/src/components/PromptModal.jsx
@@ -26,7 +26,9 @@ const PromptModal = ({
           zIndex: 1000,
         }}
       >
-        <div
+          {enhancedImage ? (
+            
+            <div
           style={{
             backgroundColor: "white",
             padding: "20px",
@@ -36,8 +38,6 @@ const PromptModal = ({
             textAlign: "center",
           }}
         >
-          {enhancedImage ? (
-            <>
               <img
                 src={enhancedImage}
                 alt="Enhanced Drawing"
@@ -75,57 +75,68 @@ const PromptModal = ({
                   Next
                 </button>
               </div>
-            </>
+            
+            </div>
           ) : (
-            <>
-              <h2 style={{ marginBottom: "20px" }}>
-              Enter a prompt for AI to enhance your drawing:
-              </h2>
-              <div
-                className="text-input"
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  gap: "10px",
-                }}
-              >
-                <input
-                  type="text"
-                  value={inputText}
-                  placeholder="eg. an elephant with pink hat"
-                  onChange={(e) => setInputText(e.target.value)}
-                  style={{
-                    width: "100%",
-                    padding: "10px",
-                    borderRadius: "5px",
-                    border: "1px solid #ccc",
-                    fontSize: "16px",
-                  }}
-                />
-                <button
-                  onClick={handleTextSubmit}
-                  disabled={isUploading}
-                  style={{
-                    width: "100%",
-                    padding: "10px",
-                    borderRadius: "5px",
-                    backgroundColor: isUploading ? "#ccc" : "black",
-                    color: "white",
-                    border: "none",
-                    fontSize: "16px",
-                    cursor: isUploading ? "not-allowed" : "pointer",
-                  }}
-                >
-                  <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg" className="buttonIcon">
-                  <path d="M1 21L14.8462 7.15385M17.9231 4.07692L19.4615 2.53846M14.0769 3.30769V1M18.6923 7.92308H21M17.1538 11L18.6923 12.5385M9.46154 3.30769L11 4.84615" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                AI Enhance
-                </button>
-              </div>
-            </>
+<>
+  <div style={{ width: "90%", maxWidth: "500px", margin: "auto auto", textAlign: "left" }}>
+    <h2 style={{ marginBottom: "20px", fontWeight: "normal", color:"white"}}>
+      Enter a prompt for AI to enhance your drawing:
+    </h2>
+    <div
+      style={{
+        backgroundColor: "white",
+        padding: "5px",
+        borderRadius: "10px",
+        width: "90%",
+        margin: "10px",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "30px", 
+      }}
+    >
+        <input
+          type="text"
+          value={inputText}
+          placeholder="eg. an elephant with pink hat"
+          onChange={(e) => setInputText(e.target.value)}
+          style={{
+            width: "90%",
+            borderRadius: "5px",
+            border: "none",
+            fontSize: "16px",
+            paddingBottom: "40px",
+            marginBottom:"10px",
+            height:"60px",
+          }}
+        />
+        <button
+          onClick={handleTextSubmit}
+          disabled={isUploading}
+          style={{
+            width: "55%",
+            padding: "15px",
+            borderRadius: "5px",
+            backgroundColor: isUploading ? "#ccc" : "black",
+            color: "white",
+            border: "none",
+            fontSize: "16px",
+            marginBottom:"10px",
+            cursor: isUploading ? "not-allowed" : "pointer",
+          }}
+        >
+          <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg" className="buttonIcon">
+            <path d="M1 21L14.8462 7.15385M17.9231 4.07692L19.4615 2.53846M14.0769 3.30769V1M18.6923 7.92308H21M17.1538 11L18.6923 12.5385M9.46154 3.30769L11 4.84615" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+          AI Enhance
+        </button>
+      </div>
+    </div>
+</>
+
           )}
-        </div>
       </div>
     )
   );
