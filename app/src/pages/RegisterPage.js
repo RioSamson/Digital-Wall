b733d7e2987d39928b30584249240267434db1b2
Sadaf <sadaf.ahmadi.1998@gmail.com>
2024-07-20 21:33:12 +0200

    updated font and size login and reg page

--- app/src/pages/RegisterPage.js
+++ app/src/pages/RegisterPage.js
@@ -41,38 +41,107 @@ export default function RegisterPage() {
         }
       }
     };
+
+    const styles = {
+      container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        background: 'white',
+        marginTop: ' 80px',
+        gap: '32px'
+      },
+      inputContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '16px',
+      },
+      input: {
+        padding: '15px',
+        width: '320px',
+        borderRadius: '10px',
+        border: 'solid 1px',
+        background: 'white',
+        fontSize: '16px',
+        boxSizing: 'border-box'
+      
+      },
+      button: {
+        padding: '12px 116px',
+        backgroundColor: 'black',
+        width: '320px',
+        height: '56px',
+        color: 'white',
+        border: 'none',
+        borderRadius: '10px',
+        fontSize: '24px',
+        boxSizing: 'border-box'
+      },
+      link: {
+        marginLeft: '5px',
+        color: 'black',
+        textDecoration: 'none',
+        fontSize: '16px',
+        fontWeight: 700
+      },
+      errorMessage: {
+        color: 'red',
+        margin: '10px'
+      },
+      h2: {
+        fontSize: '32px',
+        fontWeight: 600,
+        color: 'black',
+        textAlign: 'center'
+      },
+      highlight: {
+        color: '#F3801F',
+      },
+      newline: {
+        display: 'block'
+      }
+    };
   
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-        <h2>Hello! Register to get started</h2>
-        <input
+      <div style={styles.container}>
+        <h2 style={styles.h2}>
+        <span style={styles.highlight}>Hello!</span><span style={styles.newline}> Register to start</span>
+      </h2>
+
+      <div style={styles.inputContainer}>
+      <input
           type="name"
           placeholder="Name"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          style={{ margin: '10px', padding: '10px', width: '300px', borderRadius:"5px", border:'solid 1px' }}
+          style={styles.input}
         />
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          style={{ margin: '10px', padding: '10px', width: '300px', borderRadius:"5px", border:'solid 1px' }}
+          style={styles.input}
         />
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          style={{ margin: '10px', padding: '10px', width: '300px', borderRadius:"5px", border:'solid 1px' }}
+          style={styles.input}
         />
         <input
           type="password"
           placeholder="Confirm Password"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
-          style={{ margin: '10px', padding: '10px', width: '300px', borderRadius:"5px", border:'solid 1px' }}
+          style={styles.input}
         />
+      </div>
         {errorMessage && (
           <div style={{ color: 'red', margin: '10px' }}>
             {errorMessage}
@@ -80,14 +149,14 @@ export default function RegisterPage() {
         )}
         <button
           onClick={handleSignup}
-          style={{ margin: '30px', padding: '10px 40px',backgroundColor: 'black', color:'white', border:'none', borderRadius:'5px' }}
+          style={styles.button}
           disabled={isSigningIn}
         >
           Register
         </button>
         <div style={{ marginTop: '20px' }}>
-          <span>already have an account?</span>
-          <Link to="/login" style={{ marginLeft: '5px', color: 'orange',textDecoration: 'none'  }}>Login now</Link>
+          <span style={{fontSize: '16px', fontWeight:400}}>Already have an account?</span>
+          <Link to="/login" style={styles.link}>Login now</Link>
         </div>
         
       </div>
