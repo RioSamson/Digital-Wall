8aa487a352677bc7c7e130bc6a9e9ccd38301cd6
Rio Samson <riobenielsamson@gmail.com>
2024-07-20 21:06:50 -0700

    uploaded video

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const App = () => {
   const navigate = useNavigate();
@@ -9,24 +9,47 @@ const App = () => {
   };
 
   const handleLoginClick = () => {
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <h1 style={{ fontWeight: 'normal', marginBottom: '200px', marginTop: '20px' }}>ArtSpark</h1>
-      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h1
+        style={{
+          fontWeight: "normal",
+          marginBottom: "200px",
+          marginTop: "20px",
+        }}
+      >
+        ArtSpark
+      </h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <button
           onClick={handleLoginClick}
           style={{
-            margin: '10px',
-            padding: '15px 100px',
-            fontSize: '1.125rem', 
-            backgroundColor: 'black',
-            color: 'white',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: 'pointer'
+            margin: "10px",
+            padding: "15px 100px",
+            fontSize: "1.125rem",
+            backgroundColor: "black",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
           }}
         >
           Login
@@ -34,14 +57,14 @@ const App = () => {
         <button
           onClick={handleGuestClick}
           style={{
-            margin: '10px',
-            padding: '15px 100px',
-            fontSize: '1.125rem',
-            backgroundColor: 'white',
-            color: 'black',
-            border: '2px solid black',
-            borderRadius: '5px',
-            cursor: 'pointer'
+            margin: "10px",
+            padding: "15px 100px",
+            fontSize: "1.125rem",
+            backgroundColor: "white",
+            color: "black",
+            border: "2px solid black",
+            borderRadius: "5px",
+            cursor: "pointer",
           }}
         >
           Guest
